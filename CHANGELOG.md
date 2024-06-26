89f1e791448f846730ed2ce03d90994cd22c17b8
zhouyiqi91 <386346852@qq.com>
2024-06-26 09:49:12 +0800

    reduce memory usage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,3 +9,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Add subsample plots: saturation and median_gene.
 - Change minimum cell UMI from 1000 to 500. [4ccb884](https://github.com/singleron-RD/scrna/commit/4ccb8843e5263dfdcf4be6051480ac065ff84a9c)
 - Refactor the code to generate the HTML report.
+
+
+## v1.2.0 - [2024-06]
+
+### `Changed`
+- Reduce the memory usage of the `subsample` module.
+- 
--- bin/subsample.py
+++ bin/subsample.py
@@ -30,17 +30,19 @@ def get_records(bam_file):
     cb_int = {}
     ub_int = {}
     gx_int = {}
-    name_int = {}
     n_cb = n_ub = n_gx = n_read = 0
+    dup_align_read_names = set()
     with pysam.AlignmentFile(bam_file) as bam:
         for record in bam:
-            if record.query_name in name_int:  # already added
-                continue
             cb = record.get_tag("CB")
             ub = record.get_tag("UB")
             gx = record.get_tag("GX")
-
             if all(x != "-" for x in (cb, ub, gx)):
+                if record.get_tag("NH") > 1:
+                    if record.query_name in dup_align_read_names:
+                        continue
+                    else:
+                        dup_align_read_names.add(record.query_name)
                 # use int instead of str to avoid memory hog
                 if cb not in cb_int:
                     n_cb += 1
@@ -53,7 +55,6 @@ def get_records(bam_file):
                     gx_int[gx] = n_gx
                 a.append((cb_int[cb], ub_int[ub], gx_int[gx]))
                 n_read += 1
-                name_int[record.query_name] = n_read
     return a, cb_int
 
 
